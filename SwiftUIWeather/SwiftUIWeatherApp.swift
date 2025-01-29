307245ae83f7ab6f0920a815f2b4207107b64394
Marcelo <DEV.marceloakutsu@gmail.com>
2025-01-30 06:17:08 +0900

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by AMTK on 2025/01/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
